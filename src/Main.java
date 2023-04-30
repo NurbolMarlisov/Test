d70ef96cdb2d62fd9453aedca7ed67e3450fbc15
NurbolMarlisov <marlisaaliev195@gmail.com>
2023-04-30 13:56:41 +0600

    Повторно залил

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+String [] words = {"java ","kotlin", "python", "javascript", "nodejs", "go", "react", "react native", "swift", "dart"};
+boolean hasAndroid = false;
+for (String word : words){
+if(word.contains("android")){
+    hasAndroid = true;
+    break;
+    }
+   }
+if(hasAndroid){
+    System.out.println("Массив содержит слово android");
+}else {
+    System.out.println("Массив не содержит слово android");
+}
     }
 }
